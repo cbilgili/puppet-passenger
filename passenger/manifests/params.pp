b2fe821a4fa83371ac6aba2c6e95e472e0d327ae
Canbey Bilgili <cbilgili@gmail.com>
2014-03-10 23:46:12 +0200

    Passenger conf update

--- passenger/manifests/params.pp
+++ passenger/manifests/params.pp
@@ -2,7 +2,7 @@
 #
 #
 class passenger::params {
-  $passenger_ruby = "/usr/bin/ruby1.9.1"
+  $passenger_ruby = "/opt/ruby-2.0.0-p353/bin/ruby"
   $passenger_version = "4.0.37"
   $gem_path = "/opt/ruby-2.0.0-p353/lib/ruby/gems/2.0.0"
   $gem_binary_path = "/opt/ruby-2.0.0-p353/bin"
